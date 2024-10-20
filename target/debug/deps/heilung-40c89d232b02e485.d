7335ca88e88d1ef9b601d2639322328931a3f8f5
NW <sr.chico.source@gmail.com>
2024-10-20 15:08:08 -0300

    Hello, world!

---
+++ target/debug/deps/heilung-40c89d232b02e485.d
@@ -0,0 +1,5 @@
+C:\Users\Usuário\Documents\Lucas\Github\heilung\target\debug\deps\libheilung-40c89d232b02e485.rmeta: src\main.rs
+
+C:\Users\Usuário\Documents\Lucas\Github\heilung\target\debug\deps\heilung-40c89d232b02e485.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/heilung-d9aa37d9ff855b56.d
@@ -0,0 +1,5 @@
+C:\Users\Usuário\Documents\Lucas\Github\heilung\target\debug\deps\libheilung-d9aa37d9ff855b56.rmeta: src\main.rs
+
+C:\Users\Usuário\Documents\Lucas\Github\heilung\target\debug\deps\heilung-d9aa37d9ff855b56.d: src\main.rs
+
+src\main.rs:
